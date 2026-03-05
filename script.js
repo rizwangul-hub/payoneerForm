73413450c698469814f220cbdfb4526af69d668a
rizwangul-hub <rizwangul-hub>
2026-03-05 21:26:52 +0500

    add my form

---
+++ script.js
@@ -0,0 +1,43 @@
+function handleSignup(event) {
+  event.preventDefault();
+
+  const name = document.getElementById("signup-name").value;
+  const email = document.getElementById("signup-email").value;
+  const password = document.getElementById("signup-pass").value;
+
+  if (name && email && password) {
+    const user = { name: name, email: email, password: password };
+
+    let payoneer = JSON.parse(localStorage.getItem("payoneer")) || [];
+
+    payoneer.push(user);
+
+    localStorage.setItem("payoneer", JSON.stringify(payoneer));
+
+    alert("Signup Successful! Please Login.");
+
+    window.location.href = "login.html";
+  }
+}
+function handleLogin(e) {
+  e.preventDefault();
+
+  const email = document.getElementById("login-email").value;
+  const password = document.getElementById("login-pass").value;
+
+  const payoneer = JSON.parse(localStorage.getItem("payoneer")) || [];
+
+  const storedUser = payoneer.find(
+    (user) => user.email === email && user.password === password
+  );
+
+  if (storedUser) {
+    localStorage.setItem("isLoggedIn", "true");
+    localStorage.setItem("userName", storedUser.name);
+
+    alert("Login Successful!");
+    window.location.href = "home.html";
+  } else {
+    alert("Invalid Email or Password");
+  }
+}
